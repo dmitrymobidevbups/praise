00b0b86e3fe9b2fe568ec8650d68e9e8ca184188
Dmitry A <adjiev.dmitry@gmail.com>
2016-04-04 20:49:19 +0300

    Initial code commit

---
+++ DAbappy/app/src/main/java/com/example/sanya/dabappy/sc5.java
@@ -0,0 +1,24 @@
+package com.example.sanya.dabappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class sc5 extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.sanya.dabappy.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sc5);
+    }
+    /** Called when the user clicks the Green button */
+    public void sendMessageG(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
